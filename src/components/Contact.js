ee7c3e81c677a386db1ca81a36b242d2f1cfe9fa
Nikolina Kokor <nkokor@icloud.com>
2023-09-16 19:11:20 +0200

    contact form added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,17 @@
+import '../style/Contact.css';
+
 function Contact() {
   return (
-    <div id='contact'></div>
+    <div id='contact'>
+      <div id='message-form'>
+        <input className='sender-input' type="text" placeholder="First name"></input>
+        <input className='sender-input' type="text" placeholder="Last name"></input>
+        <input className='sender-input' type="text" placeholder="Email address"></input>
+        <input className='sender-input' type='text' placeholder='Phone number'></input>
+        <input id='message-input' type="text" placeholder="Message"></input>
+        <p id='submit-button'>Send message</p>
+      </div>
+    </div>
   );
 }
 
--- src/data/products.js
+++ src/data/products.js
@@ -4,14 +4,14 @@ const products = [
     title: 'Harry Potter and The Sorcerer\'s Stone',
     author: 'J. K. Rowling',
     price: '$19.99',
-    rating: 5
+    rating: 4.5
   },
   {
     image: 'images/product-images/chamber-of-secrets.jpg',
     title: 'Harry Potter and the Chamber of Secrets',
     author: 'J. K. Rowling',
     price: '$19.99',
-    rating: 5
+    rating: 3.6
   },
   {
     image: 'images/product-images/prisoner-of-azkaban.jpg',
@@ -25,14 +25,14 @@ const products = [
     title: 'Harry Potter and the Goblet of Fire',
     author: 'J. K. Rowling',
     price: '$19.99',
-    rating: 5
+    rating: 3.7
   },
   {
     image: 'images/product-images/order-of-the-phoenix.jpg',
     title: 'Harry Potter and the Order of the Phoenix',
     author: 'J. K. Rowling',
     price: '$19.99',
-    rating: 5
+    rating: 4.6
   },
   {
     image: 'images/product-images/half-blood-prince.jpg',
@@ -46,14 +46,14 @@ const products = [
     title: 'Harry Potter and the Deathly Hallows',
     author: 'J. K. Rowling',
     price: '$19.99',
-    rating: 5
+    rating: 4
   },
   {
     image: 'images/product-images/cursed-child.jpg',
     title: 'Harry Potter and the Cursed Child',
     author: 'J. K. Rowling',
     price: '$19.99',
-    rating: 5
+    rating: 2.4
   }
 ]
 
