a5d989d4be3260934cd791068532676c01c7f01c
Nikolina Kokor <nkokor@icloud.com>
2023-09-18 20:20:13 +0200

    product card styling

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,13 +2,20 @@ import products from "../data/products";
 import '../style/Products.css';
 
 function Products() {
-
-    const renderProducts = (
-      <div id='products'>
-      {
-        products.map((product) => (
+  const renderProducts = (
+    <div id='products'>
+    {
+      products.map((product) => (
           <div className="product-card">
-            <img className="product-image" src={product.image}></img>
+          <img className="product-image" src={product.image}></img>
+          <div className='cart-div'>
+            <div className="cart-button">
+              <p>Add to cart</p>
+              <img className='cart-icon' src='images/icons8-cart-white-30.png'></img>
+            </div>
+            <img className='heart-icon' src='images/icons8-heart-outline-50.png'></img>
+          </div>
+          <div className="product-info">
             <p className="product-title">{product.title}</p>
             <p className="product-author">{product.author}</p>
             <p className="product-price">{product.price}</p>
@@ -20,11 +27,12 @@ function Products() {
               <img className="rating-star" src='images/icons8-star-30.png'></img>
             </div>
           </div>
-        ))
-      }
-    </div>
-    )
-    return renderProducts
+        </div>
+      ))
+    }
+  </div>
+  )
+  return renderProducts
 }
 
 export default Products;
