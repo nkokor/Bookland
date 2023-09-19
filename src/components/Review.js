1ffa8883e9479386084b002ccf27a97963647d8b
Nikolina Kokor <nkokor@icloud.com>
2023-09-19 14:22:15 +0200

    customer review cards added

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,7 +1,33 @@
-function Review() {
-  return (
-    <div id='review'></div>
-  );
+import reviews from "../data/reviews";
+import '../style/Review.css';
+
+function Reviews() {
+  const renderReviews = (
+    <div id='reviews'>
+    {
+      reviews.map((review) => (
+         <div className="review-card">
+            <div className="review-user-div">
+              <img className="review-user-image" src={review.userImage}></img>
+              <div className="user-info-div">
+               <p className="review-username">{review.userName}</p>
+               <p className="review-user-role">{review.userRole}</p>
+              </div>
+            </div>
+            <p className="review-text">{review.reviewComment}</p>
+            <div className="rating-star-div">
+              <img className="review-rating-star" src='images/icons8-yellow-star-50.png'></img>
+              <img className="review-rating-star" src='images/icons8-yellow-star-50.png'></img>
+              <img className="review-rating-star" src='images/icons8-yellow-star-50.png'></img>
+              <img className="review-rating-star" src='images/icons8-yellow-star-outline-50.png'></img>
+              <img className="review-rating-star" src='images/icons8-yellow-star-outline-50.png'></img>
+            </div>
+         </div>
+      ))
+    }
+  </div>
+  )
+  return renderReviews
 }
 
-export default Review;
+export default Reviews;
---
+++ src/data/reviews.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    userImage: 'images/user-images/mary.jpg',
+    userName: 'Mary Goldberg',
+    userRole: 'Happy customer',
+    reviewComment: 'The staff is so kind and they are always here to help. Books I ordered online were all damage-free. However, the wait was 7 days long which is a bit too long for me.',
+    rating: 4
+  },
+  {
+    userImage: 'images/user-images/ian.jpg',
+    userName: 'Ian Broke',
+    userRole: 'Happy customer',
+    reviewComment: 'I am very pleased with the quality of the books I ordered. All of them were packaged properly so they didn\'t have any damage. The wait was a tiny bit longer than I would like, but totally worth it in my opinion!',
+    rating: 4
+  },
+  {
+    userImage: 'images/user-images/anne.jpg',
+    userName: 'Anne Klein',
+    userRole: 'Happy customer',
+    reviewComment: 'There is honestly not a single bad thing to say about Bookland! I live nearby so I find myself going there probably more than one should. The staff is super nice and always gives amazing recommendations if you are not sure what to get. Also, the reading nook they have in the store is a great space if you just want to disappear for a bit.',
+    rating: 5
+  },
+  {
+    userImage: 'images/user-images/martin.jpg',
+    userName: 'Martin Newban',
+    userRole: 'Happy customer',
+    reviewComment: 'Great service, great books, would prefer it if there were more store since I live so far away so I usually have to order online.',
+    rating: 4
+  },
+  {
+    userImage: 'images/user-images/nick.jpg',
+    userName: 'Nick Jones',
+    userRole: 'Happy customer',
+    reviewComment: 'The books are fine, but the staff could be nicer.',
+    rating: 3
+  }
+]
+
+module.exports = reviews
