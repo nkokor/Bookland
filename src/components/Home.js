614273cfff9dde6014f0f5c908dd97b7249f24ef
Nikolina Kokor <nkokor@icloud.com>
2023-09-21 13:27:54 +0200

    home page responsiveness fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ function Home() {
   return (
     <div id='home'>
       <img id='banner' src='images/home-banner.png'></img>
+      <img id='vertical-banner' src='images/home-banner-vertical.png'></img>
       <p id='home-header'>Let's explore the magical world of literature</p>
       <p id='home-text'>Start now by visiting our online shop or meet us today at your local Bookland!</p>
       <p id='shop-button'>Shop now</p>
