f1292bca9e490dcc4ef762bbd8ae0096c8a784d0
Nikolina Kokor <nkokor@icloud.com>
2023-09-22 11:56:00 +0200

    search bar added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import '../style/Home.css';
 function Home() {
   return (
     <div id='home'>
-      <img id='banner' src='images/home-banner.png'></img>
-      <img id='vertical-banner' src='images/home-banner-vertical.png'></img>
+      <img id='banner'></img>
+      <img id='vertical-banner'></img>
       <p id='home-header'>Let's explore the magical world of literature</p>
       <p id='home-text'>Start now by visiting our online shop or meet us today at your local Bookland!</p>
       <a id='shop-link' href='#products'>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,26 +12,43 @@ function Menu() {
   }
 
   return (
-    <div id='menu-div' className='row'>
-      <div id='logo-div' className='col-2'>
-        <img id='logo' src='images/logo.jpg'></img>
-        <div></div>
+    <div>
+
+      <div id='page-header' className='row'>
+        <div id='logo-div' className='col-2'>
+          <img id='logo' src='images/logo.jpg'></img>
+          <div></div>
+        </div>
+
+        <div id='user-div' className='col-2'>
+          <img className='icon-menu-item' src='images/icons/icons8-heart-50.png'></img>
+          <img className='icon-menu-item' src='images/icons/icons8-cart-30.png'></img>
+          <img className='icon-menu-item' src='images/icons/icons8-account-48.png'></img>
+        </div>
+      
       </div>
-      <div id='menu-items-div' className='col-8'>
-        <a className='menu-item' href='#home'>Home</a>
-        <a className='menu-item'>About</a>
-        <a className='menu-item' href='#products'>Products</a>
-        <a className='menu-item' href="#reviews">Review</a>
-        <a className='menu-item' href='#contact'>Contact</a>
+     
+      <div id='menu-div'>
+
+        <div id='menu-items-div' className='col-8'>
+          <a className='menu-item' href='#home'>Home</a>
+          <a className='menu-item'>About</a>
+          <a className='menu-item' href='#products'>Products</a>
+          <a className='menu-item' href="#reviews">Review</a>
+          <a className='menu-item' href='#contact'>Contact</a>
+        </div>
+
+        <div id='search-div'>
+          <img src='images/icons/icons8-search-30.png'></img>
+          <input type='text' placeholder='Search website...' id='search-input'></input>
+        </div>
+
+        <div className='col-8'>
+          <img id='hamburger-menu-icon' src='images/icons/icons8-hamburger-menu-50.png' onClick={() => { handleSideMenuVisibility() }}></img>
+        </div>
+
       </div>
-      <div id='user-div' className='col-2'>
-        <img className='icon-menu-item' src='images/icons/icons8-heart-50.png'></img>
-        <img className='icon-menu-item' src='images/icons/icons8-cart-30.png'></img>
-        <img className='icon-menu-item' src='images/icons/icons8-account-48.png'></img>
-      </div>
-      <div className='col-8'>
-        <img id='hamburger-menu-icon' src='images/icons/icons8-hamburger-menu-50.png' onClick={() => { handleSideMenuVisibility() }}></img>
-      </div>
+
     </div>
   );
   
