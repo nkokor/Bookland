3ae571165a2b059795d60fe8d001ed9cfd314a36
Nikolina Kokor <nkokor@icloud.com>
2023-12-30 13:13:15 +0100

    products section connected to backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BooklandAPI.apps.BooklandapiConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -6,18 +6,23 @@ import React, { useState, useEffect } from 'react';
 
 function Products() {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await fetch('http://localhost:8000/BooklandAPI/products');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
         const data = await response.json();
         setProducts(data);
       } catch (error) {
         console.error('Error fetching products:', error);
       }
     };
+
     fetchProducts();
-  }, []); 
+  }, []);
 
   const renderProducts = (
     <section id='products-page'>
